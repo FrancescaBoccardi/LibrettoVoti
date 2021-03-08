4c67f4f8a110bebf2c03e82ae82f9fa623dc9932
franc <franc@LAPTOP-TH3CBLFI.homenet.telecomitalia.it>
2021-03-08 11:20:23 +0100

    Soluzione punto 2

--- src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
+++ src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
@@ -14,6 +14,33 @@ public class Libretto {
 		voti.add(v);
 	}
 
+	/*
+	public String votiUguali(int punteggio) {
+		//calcola una stringa che contiene i voti
+		//sarà poi il chiamante a stamparli
+	}
+	
+	public List<Voto> listaVotiUguali(int punteggio){
+		List<Voto> risultato = new ArrayList<>();
+		for(Voto v : this.voti) {
+			if(v.getVoto()==punteggio) {
+				risultato.add(v);
+			}
+		}
+		return risultato;
+	}
+	*/
+	
+	
+	public Libretto votiUguali(int punteggio) {
+		Libretto risultato = new Libretto();
+		for(Voto v : this.voti) {
+			if(v.getVoto()==punteggio) {
+				risultato.add(v);
+			}
+		}
+		return risultato;
+	}
 
 	public String toString() {
 		String s = "";
